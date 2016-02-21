7131815233fe8c09ca3b11737c64446e5315fdfc
Ludovic Coues <gkr@jellycopter.net>
2016-02-21 22:14:19 +0100

    Add SSO login and basic CREST talk

--- server.py
+++ server.py
@@ -1,13 +1,60 @@
 #!/usr/bin/env python
 
+import json
+
 from flask import Flask
-from utils import templated
+from flask import redirect, url_for, session
+from flask import request as req
+from oauth2client.client import OAuth2WebServerFlow
+import httplib2
+
+from utils import templated, SessionCredStorage
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.secret_key = 'DEV_SECRET_KEY'
+
+ROOT_URL = "https://crest-tq.eveonline.com"
+
+oa2flow = OAuth2WebServerFlow(
+    client_id = "350c3b990af74ce58c2713e6e8a95c2c",
+    client_secret = "3e4rOgOsLjnrn5KfgkBZQ6l9UnI7uBfbqHRGjGOz",
+    auth_uri = "https://login.eveonline.com/oauth/authorize",
+    token_uri = "https://login.eveonline.com/oauth/token",
+    redirect_uri = "https://starroamer.mattic.org/auth",
+    scope="",
+)
 
 @app.route('/', endpoint="index")
 @templated()
 def hello():
-    print("ping")
+    try:
+        cred = SessionCredStorage().get()
+        return {"auth": "ok"}
+    except TypeError:
+        pass
+
+@app.route('/api')
+def api():
+    cred = SessionCredStorage().get()
+    path = req.args.get('path')
+    http = httplib2.Http()
+    http = cred.authorize(http)
+    (header, content) = http.request("%s%s" % (ROOT_URL, path), "GET")
+    return "<pre>%s<hr>%s" % (
+        json.dumps(header, indent=4),
+        json.dumps(json.loads(content), indent=4)
+    )
+
+@app.route('/auth/go', endpoint="auth.start")
+def auth_start():
+    return redirect(oa2flow.step1_get_authorize_url())
+
+@app.route('/auth', endpoint="auth.back")
+def auth_back():
+    code = req.args.get('code')
+    SessionCredStorage().put(oa2flow.step2_exchange(code))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+import pickle
 from functools import wraps
-from flask import request, render_template
+from flask import request, session, render_template
+from oauth2client.client import Storage
 
 def templated(template=None):
+    """Allow a flask route to return a dict that would be used to render a
+    template. The template location is either provided when using the decorator
+    or computed from the route endpoint."""
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
@@ -14,3 +19,11 @@ def templated(template=None):
             return render_template(template_name, **ctx)
         return decorated_function
     return decorator
+
+class SessionCredStorage(Storage):
+    """Take a credential object and store it into the session."""
+    def locked_put(self, cred):
+        session['_cred'] = pickle.dumps(cred)
+
+    def locked_get(self):
+        return pickle.loads(session.get('_cred'))
